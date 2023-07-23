8d701722f7a72e5edaf720e62b4ee6389e0e00ca
hieumai943 <hieunm.hrt@gmail.com>
2023-07-24 00:09:50 +0700

    Add project files.

---
+++ Controllers/MyHeroController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPINetCore6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyHeroController : ControllerBase
+    {
+       
+        private DataContext _context;
+        public MyHeroController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<MyHero>> Get()
+        {
+            return Ok( await _context.Heroes.ToListAsync());
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<MyHero>> Get(int id)
+        {
+            var hero = await _context.Heroes.FindAsync( id);
+            if (hero == null) return BadRequest("Not Found ID");
+            return Ok(hero);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<MyHero>>> AddHero(MyHero hero)
+        {
+            _context.Heroes.Add(hero);
+            await _context.SaveChangesAsync();  
+            return Ok(await _context.Heroes.ToListAsync());
+        }     
+        [HttpPut]
+        public async Task<ActionResult<List<MyHero>>> UpdateHero(MyHero request)
+        {
+            var hero = await _context.Heroes.FindAsync(request.Id);
+            if (hero == null) return BadRequest("Node found ID");   
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            await _context.SaveChangesAsync();
+
+
+            return Ok(await _context.Heroes.ToListAsync());  
+        }
+        [HttpDelete]
+        public async Task<ActionResult<List<MyHero>>> DeleteHero(MyHero request)
+        {
+            var hero = await _context.Heroes.FindAsync(request.Id);
+
+            if (hero == null) return BadRequest("Not found ID");
+
+            _context.Heroes.Remove(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Heroes.ToListAsync());
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyAPINetCore6.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<MyHero> Heroes { get; set; }
+    }
+}
